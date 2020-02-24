176e6f7602cddaeb5dc07b454074ed1cc8643762
Pranav Tomar <pranavtom1223@gmail.com>
2020-02-24 12:53:39 +0530

    done

---
+++ src/Node.java
@@ -0,0 +1,28 @@
+
+
+public class Node
+{
+    private Person data;
+    private Node next;
+
+    Node(Person data)
+    {
+        this.data = data;
+    }
+
+    public Person getData() {
+        return data;
+    }
+
+    public void setData(Person data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
